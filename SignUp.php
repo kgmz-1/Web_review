eed781c14541f785e25e2bc9fa6f02df5f34dad1
kgmz-1 <sayaan.chowdhury8523@gmail.com>
2024-12-10 00:38:35 +1100

    Add files via upload

--- SignUp.php
+++ SignUp.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json");
 require "DBConnection.php";
+require "SendEmail.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'), true);
@@ -16,6 +17,14 @@ function handlePost($conn, $input) {
     $sql = "INSERT INTO tab_user (UserName, Passwd,FName,LName,Email,Addr) VALUES ('".$input['username']."','".$passwd."','".$input['fname']."','".$input['lname']."','".$input['email']."','".$input['addr']."')";
     file_put_contents("Log.txt", $sql);
     if ($conn -> query($sql)) {
+
+        /*$text = "Please click http://localhost/WebRating/VerifyEmail.php?id=".urlencode($input['email'])." to verify your email.";
+        if(SendEmail($row["Email"],$text) == 0) {
+            echo json_encode(['message' => 'Success']);
+        }
+        else
+            echo json_encode(['message' => 'Failed']);
+        */
 		echo json_encode(['message' => 'Success']);
 	} else {
 		echo json_encode(['message' => 'Failed']);
